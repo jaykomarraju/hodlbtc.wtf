88d4579afccc8956bebfa81b262f584df54a949c
jaykomarraju <jay.komarraju@gmail.com>
2023-05-27 22:27:13 -0400

    dummyMessages update

--- block-explorer/src/components/Chat.jsx
+++ block-explorer/src/components/Chat.jsx
@@ -146,7 +146,128 @@ const ChatComponent = () => {
           content: "Crypto_Carl, good point. Let's not forget to trade wisely.",
           attachment: "www.tradewisely.com/crypto-risk-management",
         },
-      ];
+        {
+          user: "Alice_BTC",
+          time: "09:10 AM",
+          content: "Is anyone into BTC futures trading? I'd like some insight.",
+          attachment: null,
+        },
+        {
+          user: "Trader_Tina",
+          time: "09:11 AM",
+          content: "Alice_BTC, sure thing! Here's a beginners guide to futures trading.",
+          attachment: "www.tradewisely.com/btc-futures-guide",
+        },
+        {
+          user: "Miner_Mike",
+          time: "09:12 AM",
+          content: "I've noticed an increase in the hash rate recently. Any thoughts?",
+          attachment: null,
+        },
+        {
+          user: "Crypto_Bob",
+          time: "09:13 AM",
+          content: "Miner_Mike, that could be due to more miners joining the network given the price surge.",
+          attachment: null,
+        },
+        {
+          user: "Satoshi_Sammy",
+          time: "09:14 AM",
+          content: "Just to mention, I've started a thread about Bitcoin's energy consumption. Let's continue there?",
+          attachment: "www.forumlink.com/bitcoin-energy-consumption",
+        },
+        {
+          user: "Decentral_Dan",
+          time: "09:15 AM",
+          content: "Great, Satoshi_Sammy! That's an important topic to discuss.",
+          attachment: null,
+        },
+        {
+          user: "HODL_Harry",
+          time: "09:16 AM",
+          content: "Hey, guys! How's Lightning Network going? Any experience?",
+          attachment: null,
+        },
+        {
+          user: "Blockchain_Billy",
+          time: "09:17 AM",
+          content: "Lightning Network is doing great. Speeding up transactions and reducing fees.",
+          attachment: null,
+        },
+        {
+          user: "Crypto_Carl",
+          time: "09:18 AM",
+          content: "Do you guys think the bull run will last?",
+          attachment: null,
+        },
+        {
+          user: "Trader_Tina",
+          time: "09:19 AM",
+          content: "Hard to predict, Crypto_Carl. Just be prepared for any situation.",
+          attachment: null,
+        },
+        {
+          user: "Alice_BTC",
+          time: "09:20 AM",
+          content: "I think it's important to diversify investments, not just in Bitcoin but other cryptos as well.",
+          attachment: null,
+        },
+        {
+          user: "Crypto_Bob",
+          time: "09:21 AM",
+          content: "Alice_BTC, wise words. A balanced portfolio is key.",
+          attachment: null,
+        },
+        {
+          user: "Satoshi_Nakam",
+          time: "09:22 AM",
+          content: "Anyone has thoughts about Bitcoin vs. Bitcoin Cash?",
+          attachment: null,
+        },
+        {
+          user: "Blockchain_Billy",
+          time: "09:23 AM",
+          content: "Bitcoin Cash has its advantages but Bitcoin is the original and still the most widely accepted.",
+          attachment: null,
+        },
+        {
+          user: "HODL_Harry",
+          time: "09:24 AM",
+          content: "HODLing both, just in case!",
+          attachment: null,
+        },
+        {
+          user: "Miner_Mike",
+          time: "09:25 AM",
+          content: "Mining Bitcoin Cash is easier, but it's less profitable compared to Bitcoin.",
+          attachment: null,
+        },
+        {
+          user: "Decentral_Dan",
+          time: "09:26 AM",
+          content: "Don't forget that Bitcoin has much stronger network effects.",
+          attachment: null,
+        },
+        {
+          user: "Satoshi_Sammy",
+          time: "09:27 AM",
+          content: "Here's an article comparing Bitcoin and Bitcoin Cash. It might be helpful.",
+          attachment: "www.coindesk.com/bitcoin-vs-bitcoin-cash",
+        },
+        {
+          user: "Crypto_Carl",
+          time: "09:28 AM",
+          content: "I believe the market will eventually decide which one has more value.",
+          attachment: null,
+        },
+        {
+          user: "Trader_Tina",
+          time: "09:29 AM",
+          content: "Exactly, Crypto_Carl. And that's why we need to be vigilant about market trends.",
+          attachment: null,
+        },
+    ];
+    
 
   const messagesEndRef = useRef(null);
 
